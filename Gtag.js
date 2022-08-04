3d8224de813d7ece398246ed53739a145f86ee27
Amblessed <105244167+Ambie-lyon@users.noreply.github.com>
2022-08-04 15:58:46 +0100

    Update Gtag.js

--- Gtag.js
+++ Gtag.js
@@ -1,4 +1,4 @@
-type= "text/javascript" 
+type="text/javascript" 
 src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=HfEaKMSZ7KKgBRK5tB2u4R3LbY8o__pFGjs1hZD1FDFouj2p-BpXOOdWkuL32342" charset="UTF-8"
 src="https://www.googletagmanager.com/gtag/js?id=G-2SJ29ZK43S"
 window.dataLayer = window.dataLayer || [];
